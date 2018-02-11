da046a66811d0fdd2ebb0114298aa5ad420bc472
Russell Leake <gt3389b@gmail.com>
2018-02-11 10:35:30 -0500

    Add support for updating object/resource db collections

--- scripts/lwm2mid.js
+++ scripts/lwm2mid.js
@@ -25,92 +25,92 @@ var resources = [];
 
 process.on('dbison', function(){
 
-/*
-    var owner1 = new models.Owner();
-    owner1.id = 1;
-    owner1.name = "OMA LWM2M";
-    owner1.save();
+   if (0) {
+      var owner1 = new models.Owner();
+      owner1.id = 1;
+      owner1.name = "OMA LWM2M";
+      owner1.save();
 
-    var owner2 = new models.Owner();
-    owner2.id = 2;
-    owner2.name = "IPSO Alliance";
-    owner2.save();
-
-
-    _.each(lwm2mid._defs.oid, function(k, v){
-        var lwobj = new models.LwObject();
-        lwobj.id = k;
-        lwobj.shortname = v;
-        lwobj.name = "";
-        lwobj.description = "";
-        lwobj.owner = owner1._id;
-        lwobj.save();
-    });
+      var owner2 = new models.Owner();
+      owner2.id = 2;
+      owner2.name = "IPSO Alliance";
+      owner2.save();
 
 
+      // Create the objects
+      _.each(lwm2mid._defs.oid, function(k, v){
+         var lwobj = new models.LwObject();
+         lwobj.id = k;
+         lwobj.shortname = v;
+         lwobj.name = "";
+         lwobj.description = "";
+         lwobj.owner = owner1._id;
+         lwobj.save();
+      });
 
-    _.each(reusable_resources, function(k, v){
+      // Create reusable resources
+      _.each(reusable_resources, function(k, v){
+         var lwres = new models.LwResource();
+         lwres.id = v;
+         lwres.shortname = k.shortname;
+         lwres.name = "";
+         lwres.description = "";
+         lwres.access= k.access;
+         lwres.multiple= k.multi;
+         lwres.mandatory= k.mand;
+         lwres.type= k.type.charAt(0).toUpperCase() + k.type.slice(1);
+         lwres.range= k.range;
+         lwres.units= "";
 
-        var lwres = new models.LwResource();
-        lwres.id = v;
-        lwres.shortname = k.shortname;
-        lwres.name = "";
-        lwres.description = "";
-        lwres.access= k.access;
-        lwres.multiple= k.multi;
-        lwres.mandatory= k.mand;
-        lwres.type= k.type.charAt(0).toUpperCase() + k.type.slice(1);
-        lwres.range= k.range;
-        lwres.units= "";
+         lwres.save(function(err){
+               if(err){
+                  console.log(err)
+               }
+         });
+      });
+   }
 
-        lwres.save(function(err){
-            if(err){
-                console.log(err)
-            }
-        });
-    });
-*/
+   if (0) {
+      // Create specific resources and link them to an object
+      var LwObject = models.LwObject;
+      _.each(specific_resources, function(k, v){
+         var lwres = new models.LwResource();
+         lwres.id = k.id.toString();
+         lwres.shortname = v;
+         lwres.name = "";
+         lwres.description = "";
+         lwres.access= k.access;
+         lwres.multiple= k.multi;
+         lwres.mandatory= k.mand;
+         lwres.type= k.type.charAt(0).toUpperCase() + k.type.slice(1);
+         lwres.range= k.range;
+         lwres.units= "";
 
-/*
-    var LwObject = models.LwObject;
-    _.each(specific_resources, function(k, v){
+         LwObject.findOne({id: k.object_id}, function(err, data){
+               if (!err && data) {
+                  lwres.specific_object = data._id;
+                  lwres.save(function(err){
+                     if(err){
+                        console.log(err)
+                     }
+                  });
+               }
+         });
+      });
 
-        var lwres = new models.LwResource();
-        lwres.id = k.id.toString();
-        lwres.shortname = v;
-        lwres.name = "";
-        lwres.description = "";
-        lwres.access= k.access;
-        lwres.multiple= k.multi;
-        lwres.mandatory= k.mand;
-        lwres.type= k.type.charAt(0).toUpperCase() + k.type.slice(1);
-        lwres.range= k.range;
-        lwres.units= "";
+   } 
 
-        LwObject.findOne({id: k.object_id}, function(err, data){
-            lwres.specific_object = data._id;
-            lwres.save(function(err){
-                if(err){
-                    console.log(err)
-                }
-            });
-        });
+   var LwObject = models.LwObject;
+   LwObject.find().lean().exec(function(err, data){
+      objects = data;
+      exports.objects = objects;
+   });
 
-
-    });
-*/
-
-    var LwObject = models.LwObject;
-    LwObject.find().lean().exec(function(err, data){
-        objects = data;
-        exports.objects = objects;
-    });
-
-    var LwResource = models.LwResource;
-    LwResource.find({}, function(err, data){
-        resources = data;
-        exports.resources = resources;
-    });
+   var LwResource = models.LwResource;
+   LwResource.find({}, function(err, data){
+      resources = data;
+      exports.resources = resources;
+   });
 
 });
 
@@ -193,8 +193,6 @@ function getResource(oid, rid, callback)
         }
     });
 }
-
-
 
 exports.module = lwm2mid;
 exports.reusable_resources = reusable_resources;
